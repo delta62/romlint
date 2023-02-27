ed9617bfaa98324ae046a265bc3655d56d0853ab
Sam Noedel <sam.noedel@gmail.com>
2023-02-26 17:04:17 -0800

    Add depth metadata to dir walker

--- dir_walker/src/lib.rs
+++ dir_walker/src/lib.rs
@@ -10,45 +10,47 @@ use tokio_stream::wrappers::ReadDirStream;
 type MetaResult = Result<FileMeta>;
 
 pub struct FileMeta {
+    pub depth: usize,
     pub meta: Metadata,
     pub path: PathBuf,
 }
 
 impl FileMeta {
-    pub async fn from_path<P: AsRef<Path>>(path: P) -> Result<Self> {
+    pub async fn from_path<P: AsRef<Path>>(path: P, depth: usize) -> Result<Self> {
         let meta = metadata(path.as_ref()).await?;
         let path = path.as_ref().to_path_buf();
 
-        Ok(Self { meta, path })
+        Ok(Self { meta, path, depth })
     }
 
-    async fn from_dir_entry(entry: DirEntry) -> Result<Self> {
+    async fn from_dir_entry(entry: DirEntry, depth: usize) -> Result<Self> {
         let path = entry.path();
         let meta = metadata(&path).await?;
 
-        Ok(Self { meta, path })
+        Ok(Self { meta, depth, path })
     }
 }
 
 pub async fn walk(path: PathBuf) -> Result<impl Stream<Item = MetaResult>> {
-    Ok(dir(path)
+    Ok(dir(path, 0)
         .await?
-        .and_then(dir_or_singleton)
+        .and_then(move |entry| dir_or_singleton(entry, 1))
         .boxed()
         .try_flatten())
 }
 
-async fn dir<P: AsRef<Path>>(path: P) -> Result<impl Stream<Item = MetaResult>> {
+async fn dir<P: AsRef<Path>>(path: P, depth: usize) -> Result<impl Stream<Item = MetaResult>> {
     let entries = read_dir(path).await?;
-    Ok(ReadDirStream::new(entries).and_then(FileMeta::from_dir_entry))
+    Ok(ReadDirStream::new(entries)
+        .and_then(move |entry| FileMeta::from_dir_entry(entry, depth + 1)))
 }
 
-async fn dir_or_singleton(file: FileMeta) -> Result<impl Stream<Item = MetaResult>> {
+async fn dir_or_singleton(file: FileMeta, depth: usize) -> Result<impl Stream<Item = MetaResult>> {
     let is_dir = file.meta.is_dir();
 
     if is_dir {
         let path = file.path.clone();
-        let rest = dir(path).await?;
+        let rest = dir(path, depth + 1).await?;
         let stream = once(async { Ok(file) });
         Ok(stream.chain(rest).boxed())
     } else {
